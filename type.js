4b7e67a1897b00391022284639fc4b6c870821fb
isha661 <ishaluhun@gmail.com>
2026-06-12 15:54:22 +0545

    arthematic operators

--- type.js
+++ type.js
@@ -14,3 +14,21 @@ console.log(x, typeof x);
 
 x = !x
 console.log(x, typeof x);
+
+////
+let y = parseInt("123 isha");
+console.log(y);  
+
+//Arthimetic Operators +,-,/,%
+let num1 = 4
+let num2 = 2
+let result = num1 + num2
+console.log(result);
+
+//shorts
+let value = 4
+value += 2 // value = value + 2
+value ++  // post increment
+console.log(value)
+++value  // pre increment
+console.log(value)
