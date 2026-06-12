ed51a083f80819f12626f3107ba724db9aadcaa5
isha661 <ishaluhun@gmail.com>
2026-06-12 14:34:04 +0545

    data conversion

---
+++ data.js
@@ -0,0 +1,24 @@
+// learning datatype: Primitive and Object
+// primitive: num, str, boolean , null , undefined, symbol
+// object
+
+//numbers
+let num = 1275
+console.log(num * 2)
+
+let num1 = 3
+console.log(typeof num1) // typeof to check the data type of a value or variable.
+
+// String
+let firstName = "Isha \n Suchcha" //new line
+let lastName = "Rai"
+let userName = firstName + lastName //concat
+console.log(userName)
+
+//boolean
+let bool = 5 > 6
+console.log(bool)
+
+//undefined
+let omg
+console.log(typeof omg)
---
+++ type.js
@@ -0,0 +1,16 @@
+// Type Conversion & Type Coercion
+let num = 6
+console.log(num, typeof num)
+
+//
+let x 
+console.log(x, typeof x);
+
+x = 8
+console.log(x, typeof x);
+
+x = x + ""
+console.log(x, typeof x);
+
+x = !x
+console.log(x, typeof x);
