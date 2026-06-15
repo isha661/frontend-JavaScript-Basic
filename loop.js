458c20cb310d0d7f4e3c5529426377417482e4fe
isha661 <ishaluhun@gmail.com>
2026-06-15 13:42:39 +0545

    for loop

--- loop.js
+++ loop.js
@@ -1,7 +1,7 @@
 // loop in js
 // while , do while and for loop
 
-// while loop
+// while loop //nitialization, condition, increment
 let i = 0 ;
  
 while(i <= 5){
@@ -11,3 +11,16 @@ while(i <= 5){
 }
 
 // do while 
+// execute the code and then check the condition
+let j = 1;
+do{
+    console.log("hi isha",j);
+    j++;
+}while(j<=5)
+
+
+// for loop
+for(let m = 1; m <= 5 ; m++)
+{
+ console.log("hi isha");
+}
