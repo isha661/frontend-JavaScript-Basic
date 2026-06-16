f1971ec6056669b5b02c1404c3f11d05b5753004
isha661 <ishaluhun@gmail.com>
2026-06-16 07:45:26 +0545

    passing and accepting in function

--- fun.js
+++ fun.js
@@ -5,4 +5,17 @@ function greet()
 {
     console.log("hello isha goodmorning !!");
 }
-greet();
+greet();  // calling function 
+
+
+
+
+//// passing and accepting in function 
+function food(user) // creating // accpetimg  user
+{
+    return `${user} momo !!`
+
+}
+let user = "isha all time fav is ";
+let type = food(user ); // calling  //passing user 
+console.log(type); // printing 
